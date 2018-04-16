74179cf0e4065f74f282acd9e26207296e62d92a
Hung Nguyen <hungnq1989@gmail.com>
2018-04-17 01:20:05 +0300

    Add migrations for channels and messages tables

--- app/GraphQL/QueryResolver.php
+++ app/GraphQL/QueryResolver.php
@@ -6,43 +6,45 @@ namespace App\GraphQL;
  * Class QueryResolver
  * @package App\GraphQL
  */
-class QueryResolver extends AbstractResolver
+class Resolvers extends AbstractResolver
 {
+    protected static $channels = [
+        [
+            'id' => '1',
+            'name' => 'soccer',
+            'messages' => [
+                [
+                    'id' => '1',
+                    'text' => 'soccer is football',
+                ],
+                [
+                    'id' => '2',
+                    'text' => 'hello soccer world cup',
+                ]
+            ]
+        ],
+        [
+            'id' => '2',
+            'name' => 'baseball',
+            'messages' => [
+                [
+                    'id' => '3',
+                    'text' => 'baseball is life',
+                ],
+                [
+                    'id' => '4',
+                    'text' => 'hello baseball world series',
+                ]
+            ]
+        ],
+    ];
+
     /**
      * @return array
      */
     public function resolveChannels()
     {
-        return [
-            [
-                'id' => '1',
-                'name' => 'soccer',
-                'messages' => [
-                    [
-                        'id' => '1',
-                        'text' => 'soccer is football',
-                    ],
-                    [
-                        'id' => '2',
-                        'text' => 'hello soccer world cup',
-                    ]
-                ]
-            ],
-            [
-                'id' => '2',
-                'name' => 'baseball',
-                'messages' => [
-                    [
-                        'id' => '3',
-                        'text' => 'baseball is life',
-                    ],
-                    [
-                        'id' => '4',
-                        'text' => 'hello baseball world series',
-                    ]
-                ]
-            ],
-        ];
+        return self::$channels;
     }
 
     /**
@@ -53,5 +55,23 @@ class QueryResolver extends AbstractResolver
     public function resolveChannel($_, array $args)
     {
         return  collect($this->resolveChannels())->firstWhere('id', $args['id']);
+    }
+
+    /**
+     * @param $_
+     * @param array $args
+     * @return array
+     */
+    public function resolveAddChannel($_, array $args)
+    {
+        $channel =  [
+            'id' => '3',
+            'name' => $args['name'],
+            'messages' => []
+        ];
+
+        self::$channels[] = $channel;
+
+        return $channel;
     }
 }
